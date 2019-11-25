266867bf669362a807d4966558226f1c44ff4eba
urmi-21 <mrbains21@gmail.com>
2019-11-25 15:22:43 -0600

    updateSamtools

--- pyrpipe/assembly.py
+++ pyrpipe/assembly.py
@@ -6,3 +6,12 @@ Created on Mon Nov 25 15:21:01 2019
 @author: usingh
 """
 
+from myutils import *
+
+class Stringtie:
+    def __init__(self):
+        self.programName="hisat2"
+        #check if hisat2 exists
+        if not checkDep([self.programName]):
+            raise Exception("ERROR: "+ self.programName+" not found.")
+            
