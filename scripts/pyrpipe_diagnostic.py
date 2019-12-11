4dc3206921bf741844915b17db2fb486658fa5e2
urmi-21 <mrbains21@gmail.com>
2019-12-11 13:46:37 -0600

    addDiagnostic

--- scripts/pyrpipe_diagnostic.py
+++ scripts/pyrpipe_diagnostic.py
@@ -9,6 +9,23 @@ Created on Tue Dec 10 11:16:40 2019
 import sys
 import os
 import argparse
+from pyrpipe import pyrpipe_utils as pu
+
+
+
+
+
+
+
+def getCommandsFromLog(inFile):
+    with open(inFile) as f:
+        data=f.read().splitlines()
+    commands=[]
+    for l in data:
+        if l.startswith("$"):
+            commands.append(l.split("$ ")[1])
+    return commands
+
 
 """
 Valid options
@@ -31,13 +48,32 @@ parser.add_argument("--benchmark", help="increase output verbosity",action="stor
 parser.add_argument("--logfile", help="increase output verbosity",action="store")
 
 
-
-
-
-
-
 args = parser.parse_args()
 
+logFile=args.logfile
+
+if logFile is None:
+    print("--logfile is required input. Exiting.")
+    sys.exit(1)
+
+print("logfile is:"+logFile)
+
+#get log file basename
+logFileDir=pu.getFileDirectory(logFile)
+logFilename=pu.getFileName(logFile)
+basename=logFilename.split(".")[0]
+
+#print("Dir:{} fn:{} bn:{}".format(logFileDir,logFilename,basename))
+
+#check all logs exist
+cmdLog=os.path.join(logFileDir,basename+".CMD.log")
+outLog=os.path.join(logFileDir,basename+".OUT.log")
+errLog=os.path.join(logFileDir,basename+".ERR.log")
+envLog=os.path.join(logFileDir,basename+".ENV.log")
+if not pu.checkFilesExists(cmdLog,outLog,errLog,envLog):
+    print("Please check missing log files. Exiting.")
+    sys.exit(1)
+    
 
 ###start processing
 vFlag=args.verbose
@@ -48,3 +84,34 @@ if vFlag:
 
 if args.report:
     print("Generating report")
+    
+    
+if args.shell:
+    print("Generating shell script")
+    #read commands log
+    commands=getCommandsFromLog(cmdLog)
+    shebang="#!/bin/bash "
+    #write to file
+    outFile=basename+"_shell.sh"
+    f=open(outFile,"w")
+    f.write(shebang+"\n")
+    f.write("\n".join(commands))
+    
+    if vFlag:
+        print("\n\n".join(commands))
+    print("shell commands written to:"+outFile)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
