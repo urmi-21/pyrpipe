571340c53e607acf9e640f104544b9dbc2c7df3c
urmi-21 <mrbains21@gmail.com>
2019-12-11 12:10:02 -0600

    addDiagnostic

--- scripts/pyrpipe_diagnostic.py
+++ scripts/pyrpipe_diagnostic.py
@@ -10,8 +10,41 @@ import sys
 import os
 import argparse
 
-print("pyrpipe diagnostic utility")
+"""
+Valid options
+help: print help
 
-parser = argparse.ArgumentParser()
-parser.parse_args()
+report: compile a full report of the analysis
+shell: compile a shell script of all executed commands
+failed: compile a list of failed commands
+passed: compile a list of passed commands
+benchmark: generate benchmarks from logs
+"""
 
+parser = argparse.ArgumentParser(prog='pyrpipe_diagnostic',add_help=True)
+parser.add_argument("--verbose", help="increase output verbosity",action="store_true")
+parser.add_argument("--report", help="increase output verbosity",action="store_true")
+parser.add_argument("--shell", help="increase output verbosity",action="store_true")
+parser.add_argument("--failed", help="increase output verbosity",action="store_true")
+parser.add_argument("--passed", help="increase output verbosity",action="store_true")
+parser.add_argument("--benchmark", help="increase output verbosity",action="store_true")
+parser.add_argument("--logfile", help="increase output verbosity",action="store")
+
+
+
+
+
+
+
+args = parser.parse_args()
+
+
+###start processing
+vFlag=args.verbose
+
+if vFlag:
+    print("pyrpipe diagnostic utility")
+
+
+if args.report:
+    print("Generating report")
