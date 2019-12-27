3c6238c3c7ee7e748583d94a50b8a619cba09d1f
urmi-21 <mrbains21@gmail.com>
2019-12-26 18:27:08 -0600

    testdata

--- tests/test_assembly.py
+++ tests/test_assembly.py
@@ -14,7 +14,7 @@ Created on Sun Nov 24 16:24:44 2019
 @author: usingh
 """
 import pytest
-from pyrpipe import sra
+from pyrpipe import assembly
 from testingEnvironment import testSpecs
 import os
 
@@ -22,14 +22,10 @@ testVars=testSpecs()
 
 def test_assembly():
     srrID=testVars.srr
-    print ("Testing Assembly...")
     testDir=testVars.testDir
-    newOb=sra.SRA(srrID,testDir)
-    assert newOb.getSrrAccession()==srrID,"Failed SRA init"
-    assert newOb.downloadSRAFile()==True, "Failed to download SRA file"
-    assert newOb.sraFileExistsLocally()==True, "Failed to locate .sra file on disk"
-    ##test fasterq-dump
-    assert newOb.runFasterQDump(deleteSRA=True,**{"-f":"","-t":testDir})==True, "Failed FQ dump"
-    assert newOb.fastqFilesExistsLocally()==True, "Failed to locate .fastq files on disk"
-    assert newOb.sraFileExistsLocally()!=True, "Failed to delete .sra files from disk"
+    #create assemble object
+    aob=assembly.Assembly()
+    assert aob.category == "Assembler","Failed Assembly init"
     
+def test_stringtie():
+    input_bam=""
