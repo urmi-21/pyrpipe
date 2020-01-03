b9ccf4b8877bfae5a9852dccaa4e694dcac54f0a
urmi-21 <mrbains21@gmail.com>
2020-01-03 17:21:25 -0600

    updateTravis

--- tests/test_assembly.py
+++ tests/test_assembly.py
@@ -25,7 +25,7 @@ def test_assembly():
     #create assemble object
     aob=assembly.Assembly()
     assert aob.category == "Assembler","Failed Assembly init"
-    
+"""    
 def test_stringtie():
     bam=testVars.hisatSortedBam
     gtf=testVars.gtf
@@ -40,22 +40,23 @@ def test_cufflinks():
     result=cl.perform_assembly(bam,out_dir=testVars.testDir, objectid="test")
     assert pu.check_files_exist(result)==True, "Failed stringtie"
     
-    
+"""
 def test_trinityFQ():
     tr=assembly.Trinity()
     tr_opts={"--seqType":"fq","--left":testVars.fq1,
             "--right":testVars.fq2,
             "--output":testVars.testDir+"/trinity_testoutfq",
             "--max_memory":"3G"}
-    st=tr.run_trinity(**trOpts)
+    st=tr.run_trinity(**tr_opts)
     assert st==True, "Failed trinity with fq"
     
 def test_trinityBam():
+    tr=assembly.Trinity()
     bam=testVars.hisatSortedBam
     tr_opts={"--genome_guided_bam":bam,
              "genome_guided_max_intron":"10000",
             "--output":testVars.testDir+"/trinity_testoutbam",
             "--max_memory":"3G",
             "--CPU":"8"}
-    st=tr.run_trinity(**trOpts)
+    st=tr.run_trinity(**tr_opts)
     assert st==True, "Failed trinity with bam"
