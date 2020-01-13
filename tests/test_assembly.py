627d7cee900b596dbd1a91d1b979bee16df007d3
urmi-21 <mrbains21@gmail.com>
2020-01-13 13:13:29 -0600

    fixTest

--- tests/test_assembly.py
+++ tests/test_assembly.py
@@ -56,6 +56,6 @@ def test_trinityBam():
     tr_opts={"--genome_guided_bam":bam,
              "--genome_guided_max_intron":"10000",
             "--output":testVars.testDir+"/trinity_testoutbam",
-            "--max_memory":"3G"}
+            }
     st=tr.run_trinity(**tr_opts)
     assert st==True, "Failed trinity with bam"
