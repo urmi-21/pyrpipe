fe24f15289d74ec68077ad67ea2b1930b8d9c71e
urmi-21 <mrbains21@gmail.com>
2020-01-29 15:48:55 -0600

    updateSetup

--- setup.py
+++ setup.py
@@ -52,27 +52,17 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: Unix",
-    ],
-    python_requires='>=3.6',
-    
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        # 'Development Status :: 4 - Beta',
-
+        
         # Indicate who your project is intended for
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Bioinformatics',
-        #'Topic :: Scientific/Engineering :: Chemistry',
 
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7'
     ],
+    python_requires='>=3.6',
+    
 )
