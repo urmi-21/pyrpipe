e5b23c6ef4de6001a4d8401a8d991b578c5dbedc
urmi-21 <mrbains21@gmail.com>
2020-02-10 11:47:09 -0600

    update ex

--- examples/Human_annotation_snakemake/build_env.sh
+++ examples/Human_annotation_snakemake/build_env.sh
@@ -4,6 +4,8 @@ set -e
 conda env create --force -f conda_env.yml
 eval "$(conda shell.bash hook)"
 conda activate pyrpipe_human
+#update pip in case it fails
+pip3 install --user --upgrade pip
 pip install cython
 pip install -Iv mikado==1.2.4
 pip install git+https://github.com/urmi-21/pyrpipe.git
