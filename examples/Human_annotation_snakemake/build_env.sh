8ac873589a8ae76158738faf44bb1072b1aedc19
urmi-21 <mrbains21@gmail.com>
2020-02-10 11:50:06 -0600

    update env

--- examples/Human_annotation_snakemake/build_env.sh
+++ examples/Human_annotation_snakemake/build_env.sh
@@ -9,3 +9,5 @@ pip3 install --user --upgrade pip
 pip install cython
 pip install -Iv mikado==1.2.4
 pip install git+https://github.com/urmi-21/pyrpipe.git
+
+echo "Environment created. To activate, use: conda activate pyrpipe_human"
