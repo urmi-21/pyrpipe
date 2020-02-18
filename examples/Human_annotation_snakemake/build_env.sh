7339315270e1335a4759042f44e3e0d349fa8afe
urmi-21 <mrbains21@gmail.com>
2020-02-18 17:37:57 -0600

    updateTestInstructions

--- examples/Human_annotation_snakemake/build_env.sh
+++ examples/Human_annotation_snakemake/build_env.sh
@@ -5,7 +5,7 @@ conda env create --force -f conda_env.yml
 eval "$(conda shell.bash hook)"
 conda activate pyrpipe_human
 #update pip in case it fails
-pip3 install --user --upgrade pip
+pip install --user --upgrade pip
 pip install cython
 pip install -Iv mikado==1.2.4
 pip install pyrpipe --upgrade
--- tests/build_test_env.sh
+++ tests/build_test_env.sh
@@ -5,6 +5,8 @@ conda env create --force -f test_environment.yml
 eval "$(conda shell.bash hook)"
 conda activate pyrpipe_test
 #update pip in case it fails
-pip3 install --user --upgrade pip
+pip install --user --upgrade pip
 pip install cython
 pip install -Iv mikado==1.2.4
+
+echo "Environment created. To activate, use: conda activate pyrpipe_test"
