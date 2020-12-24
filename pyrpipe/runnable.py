73a1619f17b386ef0cf683bbe5455aa52b518809
urmi-21 <mrbains21@gmail.com>
2020-12-23 21:21:49 -0600

    doc strings

--- pyrpipe/runnable.py
+++ pyrpipe/runnable.py
@@ -18,6 +18,31 @@ class Runnable:
     """The runnable class
     """
     def __init__(self,*args,command=None,yaml=None,style='LINUX',deps=None,valid_args=None,**kwargs):
+        """
+        
+
+        Parameters
+        ----------
+        *args : TYPE
+            DESCRIPTION.
+        command : TYPE, optional
+            DESCRIPTION. The default is None.
+        yaml : TYPE, optional
+            DESCRIPTION. The default is None.
+        style : TYPE, optional
+            DESCRIPTION. The default is 'LINUX'.
+        deps : TYPE, optional
+            DESCRIPTION. The default is None.
+        valid_args : TYPE, optional
+            DESCRIPTION. The default is None.
+        **kwargs : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
         self._runnable=True
         self._command=command
         self._deps=deps
@@ -31,18 +56,62 @@ class Runnable:
     
     @property
     def _param_yaml(self):
+        """
+        
+
+        Returns
+        -------
+        TYPE
+            DESCRIPTION.
+
+        """
         return self.__param_yaml
     @_param_yaml.setter
     def _param_yaml(self,value):
+        """
+        
+
+        Parameters
+        ----------
+        value : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
         self.__param_yaml=value
         self.load_yaml()
             
             
     @property
     def _deps(self):
+        """
+        
+
+        Returns
+        -------
+        TYPE
+            DESCRIPTION.
+
+        """
         return self.__deps
     @_deps.setter
     def _deps(self,dep_list):
+        """
+        
+
+        Parameters
+        ----------
+        dep_list : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
         #validate command exists
         if dep_list and self.check_dependency(dep_list):
             self.__deps=dep_list
@@ -50,9 +119,36 @@ class Runnable:
         
     @property
     def _command(self):
+        """
+        
+
+        Returns
+        -------
+        TYPE
+            DESCRIPTION.
+
+        """
         return self.__command
     @_command.setter
     def _command(self,value):
+        """
+        
+
+        Parameters
+        ----------
+        value : TYPE
+            DESCRIPTION.
+
+        Raises
+        ------
+        TypeError
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
         #if already exists
         if hasattr(self,'_command') and self._command:
             raise TypeError("Can not modify _command")
@@ -63,6 +159,25 @@ class Runnable:
         
     
     def resolve_parameter(self,parameter_key,passed_value,default_value,parameter_variable):
+        """
+        
+
+        Parameters
+        ----------
+        parameter_key : TYPE
+            DESCRIPTION.
+        passed_value : TYPE
+            DESCRIPTION.
+        default_value : TYPE
+            DESCRIPTION.
+        parameter_variable : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
         #First check the passed value; then the passed **kwargs
         #None values will be ignored
         if passed_value:
@@ -75,12 +190,46 @@ class Runnable:
             self._kwargs[parameter_key]=str(default_value)
          
     def check_dependency(self,deps_list):
-            if deps_list and not pe.check_dependencies(deps_list):
-                #pu.print_boldred("ERROR. Please check dependencies for {}. Deps: {}".format(self._command," ".join(deps_list)))
-                raise OSError("CommandNotFoundException")
-            return True
+        """
+        
+
+        Parameters
+        ----------
+        deps_list : TYPE
+            DESCRIPTION.
+
+        Raises
+        ------
+        OSError
+            DESCRIPTION.
+
+        Returns
+        -------
+        bool
+            DESCRIPTION.
+
+        """
+        if deps_list and not pe.check_dependencies(deps_list):
+            #pu.print_boldred("ERROR. Please check dependencies for {}. Deps: {}".format(self._command," ".join(deps_list)))
+            raise OSError("CommandNotFoundException")
+        return True
                 
     def load_args(self,*args,**kwargs):
+        """
+        
+
+        Parameters
+        ----------
+        *args : TYPE
+            DESCRIPTION.
+        **kwargs : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
         #init the parameters for the object
         if args:
             self._args=args
@@ -92,6 +241,14 @@ class Runnable:
             self._kwargs={}
         
     def load_yaml(self):        
+        """
+        
+
+        Returns
+        -------
+        None.
+
+        """
         #read yaml parameters
         if not self._param_yaml:
             return
@@ -104,6 +261,22 @@ class Runnable:
             
             
     def verify_integrity(self,target,verbose=False):
+        """
+        
+
+        Parameters
+        ----------
+        target : TYPE
+            DESCRIPTION.
+        verbose : TYPE, optional
+            DESCRIPTION. The default is False.
+
+        Returns
+        -------
+        bool
+            DESCRIPTION.
+
+        """
         #check if lock exists
         lock_files=self.get_lock_files(target)
         if len(lock_files) >0:
@@ -116,6 +289,20 @@ class Runnable:
         return True
     
     def get_lock_files(self,target):
+        """
+        
+
+        Parameters
+        ----------
+        target : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        lock_files : TYPE
+            DESCRIPTION.
+
+        """
         #check if lock exists
         filepath=pu.get_file_directory(target)
         filename=pu.get_filename(target)
@@ -127,6 +314,22 @@ class Runnable:
         
             
     def verify_target(self,target,verbose=False):
+        """
+        
+
+        Parameters
+        ----------
+        target : TYPE
+            DESCRIPTION.
+        verbose : TYPE, optional
+            DESCRIPTION. The default is False.
+
+        Returns
+        -------
+        bool
+            DESCRIPTION.
+
+        """
         if not pu.check_files_exist(target):
             if verbose: pu.print_boldred("Error: target {} not found".format(target))
             return False
@@ -135,6 +338,22 @@ class Runnable:
         
     
     def verify_target_list(self,target_list,verbose=False):
+        """
+        
+
+        Parameters
+        ----------
+        target_list : TYPE
+            DESCRIPTION.
+        verbose : TYPE, optional
+            DESCRIPTION. The default is False.
+
+        Returns
+        -------
+        bool
+            DESCRIPTION.
+
+        """
         #nothing to verify
         if not target_list:
             return True
@@ -145,6 +364,22 @@ class Runnable:
     
     
     def create_lock(self,target_list,message):
+        """
+        
+
+        Parameters
+        ----------
+        target_list : TYPE
+            DESCRIPTION.
+        message : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        templist : TYPE
+            DESCRIPTION.
+
+        """
         templist=[]
         for f in target_list:
             temp_path=pu.get_file_directory(f)
@@ -159,18 +394,80 @@ class Runnable:
         return templist
     
     def remove_locks(self,file_list):
+        """
+        
+
+        Parameters
+        ----------
+        file_list : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
         for f in file_list:
             os.remove(f)
             
             
     
     def get_valid_parameters(self,subcommand):
+        """
+        
+
+        Parameters
+        ----------
+        subcommand : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        TYPE
+            DESCRIPTION.
+
+        """
         if subcommand:
             return self._valid_args[subcommand]
         return self._valid_args
         pass
         
     def run(self,*args, subcommand=None, target=None, requires=None, objectid=None, **kwargs):
+        """
+        
+
+        Parameters
+        ----------
+        *args : Tuple
+            Positoinal arguments passed to a command. This will copmletely REPLACE the exsiting self._args created during initialization of the runnable object.
+        subcommand : String or List, optional
+            DESCRIPTION. subcommand passed to the command. The default is None.
+        target : Str or List of Str, optional
+            DESCRIPTION. The expected output/target files produced by the run operation. False  is returned is all target files are not found after the command. The default is None.
+        requires : Str or List of Str, optional
+            DESCRIPTION. Files required to strat the run method. Exception is thrown if files are missing. The default is None.
+        objectid : Str, optional
+            DESCRIPTION. A uniq id to identify the run operation in the logs. Thi is useful for benchmarks. The default is None.
+        **kwargs : Keyword arguments
+            DESCRIPTION. The options to be passed to the command. This will OVERRIDE ANY EXISTING options in the self._kwargs created during initialization of the runnable object.
+
+        Raises
+        ------
+        TypeError
+            If incorerct types are used for target and required.
+        FileNotFoundError
+            Raises FileNotFoundError if any of the required files are missing.
+        OSError
+            Raises OSError if the command is incorrect or not present in path.
+        ValueError
+            Raises ValueError if args_type is something other than LINUX or JAVA.
+
+        Returns
+        -------
+        bool
+            Return the status of command as True or False. True implies command had 0 exit-code and all target files were found after the command finished.
+
+        """
         
         #create target list
         target_list=[]
